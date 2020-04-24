2fb7b6edf70d363a8476ca8567d3eaa964d53232
Sully Fischer <fischersully@gmail.com>
2020-04-24 15:27:57 +0430

    add *.pickle files to gitignore

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.21'
+VERSION = '0.0.23'
 DESCRIPTION = "A trading framework for cryptocurrencies"
 REQUIRED_PACKAGES = [
     'psycopg2-binary',
