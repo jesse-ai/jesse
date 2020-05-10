136ae029ea8a76d05eb1700c2448474c253e49ca
Sully Fischer <fischersully@gmail.com>
2020-05-10 20:05:00 +0430

    increase version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.3.4'
+VERSION = '0.4.0'
 DESCRIPTION = "A trading framework for cryptocurrencies"
 REQUIRED_PACKAGES = [
     'psycopg2-binary',
