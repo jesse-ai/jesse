36c32077b84ea1c12810ac7dc29b576ae71d8549
lemarkus <mysecondworld@firesurf.de>
2020-05-29 21:53:29 +0200

    Min Max Extrema Indicator

--- jesse/indicators/minmax.py
+++ jesse/indicators/minmax.py
@@ -3,15 +3,15 @@ from scipy.signal import argrelextrema
 
 from collections import namedtuple
 
-EXTREMA = namedtuple('EXTREMA', ['min', 'max'])
+EXTREMA = namedtuple('EXTREMA', ['min', 'max', 'last_min', 'last_max'])
 
 
 def minmax(candles: np.ndarray, order=3, sequential=False) -> EXTREMA:
     """
-    minmax
+    minmax - get extrema
 
     :param candles: np.ndarray
-    :param mode: int - default = 0
+    :param order: int - default = 3
     :param sequential: bool - default=False
 
     :return: float | np.ndarray
@@ -28,10 +28,25 @@ def minmax(candles: np.ndarray, order=3, sequential=False) -> EXTREMA:
     min = np.full_like(low, np.nan)
     max = np.full_like(high, np.nan)
 
+    # set the extremas with the matching price
     min[minimaIdxs] = low[minimaIdxs]
     max[maximaIdxs] = high[maximaIdxs]
 
+    # forward fill Nan values to get the last extrema
+    last_min = np_ffill(min)
+    last_max = np_ffill(max)
+
     if sequential:
-        return EXTREMA(min, max)
+        return EXTREMA(min, max, last_min, last_max)
     else:
-        return EXTREMA(min[-1], max[-1])
+        return EXTREMA(min[-1], max[-1], last_min[-1], last_max[-1])
+
+def np_ffill(arr, axis=0):
+    idx_shape = tuple([slice(None)] + [np.newaxis] * (len(arr.shape) - axis - 1))
+    idx = np.where(~np.isnan(arr), np.arange(arr.shape[axis])[idx_shape], 0)
+    np.maximum.accumulate(idx, axis=axis, out=idx)
+    slc = [np.arange(k)[tuple([slice(None) if dim==i else np.newaxis
+        for dim in range(len(arr.shape))])]
+        for i, k in enumerate(arr.shape)]
+    slc[axis] = idx
+    return arr[tuple(slc)]
