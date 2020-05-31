a5792ce126a8fc8b3b3dab1435c130f7a0eea529
lemarkus <mysecondworld@firesurf.de>
2020-05-31 16:21:55 +0200

    Added source_type parameter.

--- jesse/indicators/rsi.py
+++ jesse/indicators/rsi.py
@@ -1,15 +1,16 @@
 import numpy as np
 import talib
-
+from jesse.helpers import get_candle_source
 from typing import Union
 
 
-def rsi(candles: np.ndarray, period=14, sequential=False) -> Union[float, np.ndarray]:
+def rsi(candles: np.ndarray, period=14, source_type="close", sequential=False) -> Union[float, np.ndarray]:
     """
     RSI - Relative Strength Index
 
     :param candles: np.ndarray
     :param period: int - default: 14
+    :param source_type: str - default: "close"
     :param sequential: bool - default=False
 
     :return: float | np.ndarray
@@ -17,6 +18,7 @@ def rsi(candles: np.ndarray, period=14, sequential=False) -> Union[float, np.nda
     if not sequential and len(candles) > 240:
         candles = candles[-240:]
 
-    r = talib.RSI(candles[:, 2], timeperiod=period)
+    source = get_candle_source(candles, source_type=source_type)
+    r = talib.RSI(source, timeperiod=period)
 
     return r if sequential else r[-1]
--- jesse/indicators/smma.py
+++ jesse/indicators/smma.py
@@ -1,15 +1,15 @@
 import numpy as np
-import talib
-
+from jesse.helpers import get_candle_source
 from typing import Union
 
 
-def smma(candles: np.ndarray, period=5, sequential=False) -> Union[float, np.ndarray]:
+def smma(candles: np.ndarray, period=5, source_type="close", sequential=False) -> Union[float, np.ndarray]:
     """
     SMMA - Smoothed Moving Average
 
     :param candles: np.ndarray
     :param period: int - default: 5
+    :param source_type: str - default: "close"
     :param sequential: bool - default=False
 
     :return: float | np.ndarray
@@ -17,8 +17,8 @@ def smma(candles: np.ndarray, period=5, sequential=False) -> Union[float, np.nda
     if not sequential and len(candles) > 240:
         candles = candles[-240:]
 
-    # calculate the smoothed moving average
-    res = numpy_ewma(candles[:, 2], period)
+    source = get_candle_source(candles, source_type=source_type)
+    res = numpy_ewma(source, period)
 
     return res if sequential else res[-1]
 
