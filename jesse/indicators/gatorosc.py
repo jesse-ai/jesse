3349e46fa190fad04e119ad9c9bd7207976ce40c
lemarkus <mysecondworld@firesurf.de>
2020-05-31 15:58:39 +0200

    Added source_type parameter.

--- jesse/indicators/gatorosc.py
+++ jesse/indicators/gatorosc.py
@@ -1,6 +1,6 @@
 import numpy as np
 from .smma import smma
-
+from jesse.helpers import get_candle_source
 import talib
 
 from collections import namedtuple
@@ -8,21 +8,25 @@ from collections import namedtuple
 GATOR = namedtuple('GATOR', ['upper', 'lower', 'upper_change', 'lower_change'])
 
 
-def gatorosc(candles: np.ndarray, sequential=False) -> GATOR:
+def gatorosc(candles: np.ndarray, source_type="close", sequential=False) -> GATOR:
     """
     Gator Oscillator by Bill M. Williams
 
     :param candles: np.ndarray
+    :param source_type: str - default: "close"
     :param sequential: bool - default=False
 
     :return: float | np.ndarray
     """
+
     if not sequential and len(candles) > 240:
         candles = candles[-240:]
 
-    jaw = shift(smma(candles, period=13, sequential=True), 8)
-    teeth = shift(smma(candles, period=8, sequential=True), 5)
-    lips = shift(smma(candles, period=5, sequential=True), 3)
+    source = get_candle_source(candles, source_type=source_type)
+
+    jaw = shift(smma(source, period=13, sequential=True), 8)
+    teeth = shift(smma(source, period=8, sequential=True), 5)
+    lips = shift(smma(source, period=5, sequential=True), 3)
 
     upper = np.abs(jaw - teeth)
     lower = -np.abs(teeth - lips)
