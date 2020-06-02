Added source_type and talib in calculation. Small sequential fix.

jesse/indicators/keltner.py
@@ -1,27 +1,38 @@
 import numpy as np
 import talib
-
+from jesse.helpers import get_candle_source
 from collections import namedtuple
 
-from .ema import ema
-from .atr import atr
 
 KeltnerChannel = namedtuple('KeltnerChannel', ['upperband', 'middleband', 'lowerband'])
 
-def keltner(candles: np.ndarray, period=20, multiplier=2, sequential=False) -> KeltnerChannel:
+def keltner(candles: np.ndarray, period=20, multiplier=2, source_type="close", sequential=False) -> KeltnerChannel:
     """
     Keltner Channels
 
     :param candles: np.ndarray
     :param period: int - default: 20
     :param multiplier: int - default: 2
+    :param source_type: str - default: "close"
     :param sequential: bool - default=False
 
     :return: KeltnerChannel
     """
 
-    e = ema(candles, period=period, sequential=sequential)
-    a = atr(candles, period=period, sequential=sequential)
+    if not sequential and len(candles) > 240:
+        candles = candles[-240:]
 
-    return KeltnerChannel(e + a * multiplier, e, e - a * multiplier)
+    source = get_candle_source(candles, source_type=source_type)
+    e = talib.EMA(source, timeperiod=period)
+    a = talib.ATR(candles[:, 3], candles[:, 4], candles[:, 2], timeperiod=period)
+
+    up = e + a * multiplier
+    mid = e
+    low = e - a * multiplier
+
+    if sequential:
+        return KeltnerChannel(up, mid, low)
+    else:
+        return KeltnerChannel(up[-1], mid[-1], low[-1])
+
 