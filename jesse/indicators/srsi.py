9fcaf5126599007ed2ea2c4421782bd2eb13b273
Gabriele Guidi <gabriele.guidi@injenia.it>
2020-06-15 08:55:37 +0200

    adding additional parameters to srsi indicator

--- jesse/indicators/srsi.py
+++ jesse/indicators/srsi.py
@@ -9,12 +9,15 @@ from jesse.helpers import get_candle_source
 StochasticRSI = namedtuple('StochasticRSI', ['k', 'd'])
 
 
-def srsi(candles: np.ndarray, period=14, source_type="close", sequential=False) -> StochasticRSI:
+def srsi(candles: np.ndarray, period=14, pct_k_period=14, pct_k_slowing_period=3, pct_d_period=3, source_type="close", sequential=False) -> StochasticRSI:
     """
     Stochastic RSI
 
     :param candles: np.ndarray
     :param period: int - default: 14
+    :param pct_k_period: int - default: 14
+    :param pct_k_slowing_period: int - default: 3
+    :param pct_d_period: int - default: 3
     :param source_type: str - default: "close"
     :param sequential: bool - default=False
 
@@ -26,7 +29,7 @@ def srsi(candles: np.ndarray, period=14, source_type="close", sequential=False) 
     source = get_candle_source(candles, source_type=source_type)
     rsi_np = talib.RSI(source, timeperiod=period)
     rsi_np = rsi_np[np.logical_not(np.isnan(rsi_np))]
-    fast_k, fast_d = ti.stoch(rsi_np, rsi_np, rsi_np, 14, 3, 3)
+    fast_k, fast_d = ti.stoch(rsi_np, rsi_np, rsi_np, pct_k_period, pct_k_slowing_period, pct_d_period)
 
     if sequential:
         fast_k = np.concatenate((np.full((candles.shape[0] - fast_k.shape[0]), np.nan), fast_k), axis=0)
