cff190f1bdd5ee322f122bee6844f5c38f2a95a7
Sully Fischer <fischersully@gmail.com>
2020-06-15 17:47:54 +0430

    increase version to 0.7.1

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.7.0'
+VERSION = '0.7.1'
 DESCRIPTION = "A trading framework for cryptocurrencies"
 REQUIRED_PACKAGES = [
     'psycopg2-binary',
