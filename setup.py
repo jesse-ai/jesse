47912b095cfd8567f68bd429e23eb06611c9e4b3
Saleh Mirnezami <algo@hey.com>
2020-11-13 18:25:18 +0330

    bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.13.1'
+VERSION = '0.13.2'
 DESCRIPTION = "A trading framework for cryptocurrencies"
 REQUIRED_PACKAGES = [
     'psycopg2-binary',
