49a1ac46715682e8a30df133ce055bf2dfdedb7d
Saleh Mir <algo@hey.com>
2021-01-08 14:07:14 +0100

    Add unit test: test_negative_balance_validation_for_spot_market

--- tests/test_exchange.py
+++ tests/test_exchange.py
@@ -1,42 +1,13 @@
-# import pytest
-#
-# import jesse.services.selectors as selectors
-# from jesse.config import config, reset_config
-# from jesse.enums import exchanges
-# from jesse.exceptions import NegativeBalance
-# from jesse.store import store
-#
-#
-# def set_up():
-#     reset_config()
-#     config['app']['considering_exchanges'] = [exchanges.SANDBOX]
-#     config['app']['trading_exchanges'] = [exchanges.SANDBOX]
-#     config['env']['exchanges'][exchanges.SANDBOX]['starting_balance'] = 2000
-#     store.reset()
-#
-#
-# def test_decrease_balance():
-#     set_up()
-#
-#     e = selectors.get_exchange(exchanges.SANDBOX)
-#     assert e.balance == 2000
-#     e.decrease_balance(None, 100)
-#     assert e.balance == 1900
-#
-#
-# def test_increase_balance():
-#     set_up()
-#
-#     e = selectors.get_exchange(exchanges.SANDBOX)
-#     assert e.balance == 2000
-#     e.increase_balance(None, 100)
-#     assert e.balance == 2100
-#
-#
-# def test_negative_balance_validation():
-#     with pytest.raises(NegativeBalance):
-#         set_up()
-#
-#         e = selectors.get_exchange(exchanges.SANDBOX)
-#         assert e.balance == 2000
-#         e.decrease_balance(None, 3000)
+import pytest
+
+from jesse.exceptions import NegativeBalance
+from .utils import single_route_backtest
+
+
+def test_negative_balance_validation_for_spot_market():
+    """
+    The initial account balance for USDT is 10_000. So trying to spend 10_001
+    should throw NegativeBalance exception
+    """
+    with pytest.raises(NegativeBalance):
+        single_route_backtest('TestSpotNegativeBalance', is_margin_trading=False)
