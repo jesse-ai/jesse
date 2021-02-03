42bf7baf8d5b78c53341452a8ae47e37d68e90fd
Saleh Mir <algo@hey.com>
2021-02-03 11:18:34 +0100

    use postgres_ext driver for db to have access to PostgreSQL specific features

--- jesse/services/db.py
+++ jesse/services/db.py
@@ -1,10 +1,10 @@
-import peewee
+from playhouse.postgres_ext import PostgresqlExtDatabase
 
 import jesse.helpers as jh
 
 if not jh.is_unit_testing():
     # connect to the database
-    db = peewee.PostgresqlDatabase(jh.get_config('env.databases.postgres_name'),
+    db = PostgresqlExtDatabase(jh.get_config('env.databases.postgres_name'),
                                    user=jh.get_config('env.databases.postgres_username'),
                                    password=jh.get_config('env.databases.postgres_password'),
                                    host=str(jh.get_config('env.databases.postgres_host')),
