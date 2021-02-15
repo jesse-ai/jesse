0858bbac3b9218919c4e132b01769eaeaaa90c22
Markus <mysecondworld@firesurf.de>
2021-02-15 14:26:48 +0100

    Use numba to speed up CG.

--- jesse/indicators/cg.py
+++ jesse/indicators/cg.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 import numpy as np
+from numba import njit
 
 from jesse.helpers import get_candle_source
 from jesse.helpers import get_config
@@ -23,6 +24,13 @@ def cg(candles: np.ndarray, period: int = 10, source_type: str = "close", sequen
         candles = candles[-warmup_candles_num:]
 
     source = get_candle_source(candles, source_type=source_type)
+    res = go_fast(source, period)
+
+    return np.concatenate((np.full((candles.shape[0] - res.shape[0]), np.nan), res), axis=0) if sequential else res[-1]
+
+
+@njit
+def go_fast(source, period):  # Function is compiled to machine code when called the first time
     res = np.full_like(source, fill_value=np.nan)
     for i in range(0, len(source)):
         if i > period:
@@ -35,5 +43,4 @@ def cg(candles: np.ndarray, period: int = 10, source_type: str = "close", sequen
                     denom = denom + close
             result = -num / denom if denom != 0 else 0
             res[i] = result
-
-    return np.concatenate((np.full((candles.shape[0] - res.shape[0]), np.nan), res), axis=0) if sequential else res[-1]
+    return res
