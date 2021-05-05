82cf1755e09982abbb21cdd5e51ef56e84074d29
sbkhosh <sbkhosh@gmail.com>
2021-05-05 11:37:00 +0300

    add trading_year_days option for 365 days trading for html report

--- jesse/services/quantstats.py
+++ jesse/services/quantstats.py
@@ -31,9 +31,9 @@ def quantstats_tearsheet(buy_and_hold_returns: pd.Series) -> None:
     title = f"{modes[mode][1]} → {arrow.utcnow().strftime('%d %b, %Y %H:%M:%S')}"
 
     try:
-        qs.reports.html(returns=returns_time_series, benchmark=buy_and_hold_returns, title=title, output=file_path)
+        qs.reports.html(returns=returns_time_series, trading_year_days=365, benchmark=buy_and_hold_returns, title=title, output=file_path)
     except IndexError:
-        qs.reports.html(returns=returns_time_series, title=title, output=file_path)
+        qs.reports.html(returns=returns_time_series, trading_year_days=365, title=title, output=file_path)
     except:
         raise
 
