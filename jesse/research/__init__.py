94f23a37d02aea4215dd8438450a0061222da71b
Saleh Mir <algo@hey.com>
2021-06-11 12:05:05 +0430

    implement a store_candles() function for the research module

--- jesse/research/__init__.py
+++ jesse/research/__init__.py
@@ -1,20 +1,13 @@
 from .get_candles import get_candles
+from jesse.services.db import store_candles as store_candles_from_list
+import jesse.helpers as jh
+import numpy as np
 
 
 def init() -> None:
     from pydoc import locate
     import os
     import sys
-    import jesse.helpers as jh
-
-    # Python version validation.
-    if jh.python_version() < 3.6:
-        print(
-            jh.color(
-                f'Jesse has not beed tested with your Python version ({jh.python_version()}), hence it may not work properly. Consider upgrading to >= 3.7',
-                'red'
-            )
-        )
 
     # fix directory issue
     sys.path.insert(0, os.getcwd())
@@ -34,3 +27,22 @@ def init() -> None:
         local_config = locate('config.config')
         from jesse.config import set_config
         set_config(local_config)
+
+
+def store_candles(candles: np.ndarray, exchange: str, symbol: str) -> None:
+    arr = []
+
+    for c in candles:
+        arr.append({
+            'id': jh.generate_unique_id(),
+            'symbol': symbol,
+            'exchange': exchange,
+            'timestamp': c[0],
+            'open': c[1],
+            'close': c[2],
+            'high': c[3],
+            'low': c[4],
+            'volume': c[5]
+        })
+
+    store_candles_from_list(arr)
