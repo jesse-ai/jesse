361a6d5368afb74aa515be00082d3c53327fc3c9
Saleh Mir <algo@hey.com>
2021-06-21 21:47:52 +0430

    fix app_mode in live modes changing because of import-candles

--- jesse/modes/import_candles_mode/__init__.py
+++ jesse/modes/import_candles_mode/__init__.py
@@ -18,8 +18,8 @@ from jesse.services.failure import register_custom_exception_handler
 from jesse.services.redis import sync_publish
 
 
-def run(exchange: str, symbol: str, start_date_str: str, skip_confirmation: bool = False) -> None:
-    config['app']['trading_mode'] = 'import-candles'
+def run(exchange: str, symbol: str, start_date_str: str, skip_confirmation: bool = False, mode: str = 'import-candles') -> None:
+    config['app']['trading_mode'] = mode
 
     register_custom_exception_handler()
 
