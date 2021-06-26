dc00eca89551e84a3958187fde773e2c57c44547
Saleh Mir <algo@hey.com>
2021-06-26 17:07:54 +0430

    fix CORS issue

--- jesse/services/web.py
+++ jesse/services/web.py
@@ -1,4 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 fastapi_app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+]
+
+fastapi_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
