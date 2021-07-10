816ea0ca02a2a86b0cac13b6173fa162af3a1277
Saleh Mir <algo@hey.com>
2021-07-10 20:06:50 +0430

    refactor property and method names

--- jesse/services/multiprocessing.py
+++ jesse/services/multiprocessing.py
@@ -4,27 +4,24 @@ from multiprocessing import Process
 
 class ProcessManager:
     def __init__(self):
-        self.workers: List[Process] = []
-        self.pid_to_client_id_map = {}
+        self._workers: List[Process] = []
+        self._pid_to_client_id_map = {}
         self.client_id_to_pid_to_map = {}
 
-    def reset(self):
-        self.workers = []
-        self.pid_to_client_id_map = {}
+    def _reset(self):
+        self._workers = []
+        self._pid_to_client_id_map = {}
         self.client_id_to_pid_to_map = {}
-
-    def cleanup(self):
-        self.workers = [w for w in self.workers if w.is_alive()]
 
     def add_task(self, function, client_id, *args):
         w = Process(target=function, args=args)
-        self.workers.append(w)
+        self._workers.append(w)
         w.start()
-        self.pid_to_client_id_map[w.pid] = client_id
+        self._pid_to_client_id_map[w.pid] = client_id
         self.client_id_to_pid_to_map[client_id] = w.pid
 
     def get_client_id(self, pid):
-        client_id: str = self.pid_to_client_id_map[pid]
+        client_id: str = self._pid_to_client_id_map[pid]
         # return after "-" because we add them before sending it to multiprocessing
         return client_id[client_id.index('-') + len('-'):]
 
@@ -33,22 +30,22 @@ class ProcessManager:
 
     def cancel_process(self, client_id):
         pid = self.get_pid(client_id)
-        for i, w in enumerate(self.workers):
+        for i, w in enumerate(self._workers):
             if w.is_alive() and w.pid == pid:
                 del self.client_id_to_pid_to_map[client_id]
-                del self.pid_to_client_id_map[w.pid]
+                del self._pid_to_client_id_map[w.pid]
                 w.terminate()
                 w.join()
                 w.close()
-                del self.workers[i]
+                del self._workers[i]
                 return
 
     def flush(self):
-        for w in self.workers:
+        for w in self._workers:
             w.terminate()
             w.join()
             w.close()
-        process_manager.reset()
+        process_manager._reset()
 
 
 process_manager = ProcessManager()
