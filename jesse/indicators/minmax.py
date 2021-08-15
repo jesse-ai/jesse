bd66d4f93b2ef2196a4e0321b08fdc716468fb90
Markus K <16636744+cryptocoinserver@users.noreply.github.com>
2021-08-15 14:11:39 +0200

    Update minmax.py

--- jesse/indicators/minmax.py
+++ jesse/indicators/minmax.py
@@ -40,4 +40,4 @@ def minmax(candles: np.ndarray, order: int = 3, sequential: bool = False) -> EXT
     if sequential:
         return EXTREMA(is_min, is_max, last_min, last_max)
     else:
-        return EXTREMA(is_min[-1], is_max[-1], last_min[-1], last_max[-1])
+        return EXTREMA(is_min[-(order+1)], is_max[-(order+1)], last_min[-1], last_max[-1])
