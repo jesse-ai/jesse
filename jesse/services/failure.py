29d421e31aef245161313d359fe93b3553275463
Saleh Mir <algo@hey.com>
2021-08-22 12:05:19 +0430

    remove comments and debugging code

--- jesse/services/failure.py
+++ jesse/services/failure.py
@@ -27,36 +27,8 @@ def register_custom_exception_handler() -> None:
     else:
         logging.basicConfig(level=logging.INFO)
 
-    # TODO: for the actual main thread it doesn't work. Should we worry?
-    # # main thread
-    # def handle_exception(exc_type, exc_value, exc_traceback) -> None:
-    #     print('====')
-    #     print('oops error in the MAIN thread')
-    #     print('====')
-    #
-    #     if issubclass(exc_type, KeyboardInterrupt):
-    #         sys.excepthook(exc_type, exc_value, exc_traceback)
-    #         return
-    #
-    #     # send notifications if it's a live session
-    #     if jh.is_live():
-    #         jesse_logger.error(
-    #             f'{exc_type.__name__}: {exc_value}'
-    #         )
-    #
-    #     sync_publish('exception', {
-    #         'error': f"{type(exc_type.__name__)}: {str(exc_value)}",
-    #         'traceback': str(traceback.format_exc())
-    #     })
-    #
-    # sys.excepthook = handle_exception
-
     # other threads
     def handle_thread_exception(args) -> None:
-        print('\n')
-        print('other threads')
-        print('\n')
-
         if args.exc_type == SystemExit:
             return
 
@@ -70,9 +42,6 @@ def register_custom_exception_handler() -> None:
             'error': f"{args.exc_type.__name__}: {str(args.exc_value)}",
             'traceback': str(traceback.format_exc())
         })
-
-        print('Unhandled exception in the thread:')
-        print(traceback.format_exc())
 
         terminate_session()
 
