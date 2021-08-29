ece99024ef8f8c7ff5e038a73843fae2a8569778
Markus <mysecondworld@firesurf.de>
2021-08-29 13:46:12 +0200

    Make db connection more stable.

--- jesse/services/db.py
+++ jesse/services/db.py
@@ -4,12 +4,21 @@ from typing import Dict, List
 import jesse.helpers as jh
 
 if not jh.is_unit_testing():
+
+    keepalive_kwargs = {
+        "keepalives": 1,
+        "keepalives_idle": 60,
+        "keepalives_interval": 10,
+        "keepalives_count": 5
+    }
+
     # connect to the database
     db = PostgresqlExtDatabase(jh.get_config('env.databases.postgres_name'),
                                user=jh.get_config('env.databases.postgres_username'),
                                password=jh.get_config('env.databases.postgres_password'),
                                host=str(jh.get_config('env.databases.postgres_host')),
-                               port=int(jh.get_config('env.databases.postgres_port')))
+                               port=int(jh.get_config('env.databases.postgres_port')),
+                               **keepalive_kwargs)
 
 
     def close_connection() -> None:
