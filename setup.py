629be96aa42c58d4ff5a013fa4e16dfd510eef8e
Saleh Mir <algo@hey.com>
2021-10-12 16:28:39 +0200

    final fix for "no candles were passed?"

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # also change in version.py
-VERSION = '0.27.10'
+VERSION = '0.27.11'
 DESCRIPTION = "A trading framework for cryptocurrencies"
 
 REQUIRED_PACKAGES = [
