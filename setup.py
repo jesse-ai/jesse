7130a38ab51dee25e39809d1514ad9ac8c5f055c
Saleh Mir <algo@hey.com>
2021-10-23 21:51:24 +0200

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # also change in version.py
-VERSION = '0.28.0'
+VERSION = '0.28.1'
 DESCRIPTION = "A trading framework for cryptocurrencies"
 
 REQUIRED_PACKAGES = [
