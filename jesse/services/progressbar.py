33d954b78c4645323863c0040d25b1a814301238
Saleh Mir <algo@hey.com>
2021-10-27 18:37:56 +0200

    add better hanlding of "finished" cases

--- jesse/services/progressbar.py
+++ jesse/services/progressbar.py
@@ -7,18 +7,29 @@ class Progressbar:
     def __init__(self, length: int, step=1):
         self.length = length
         self.index = 0
+
+        # validation
+        if self.length <= self.index:
+            raise ValueError('length must be greater than 0')
+
         self._time = time()
-        self._execution_times = DynamicNumpyArray((100, 1), 100)
+        self._execution_times = DynamicNumpyArray((3, 1), 3)
         self.step = step
+        self.is_finished = False
 
     def update(self):
-        self.index += self.step
+        if not self.is_finished:
+            self.index += self.step
+            if self.index == self.length:
+                self.is_finished = True
         now = time()
         self._execution_times.append(np.array([now - self._time]))
         self._time = now
 
     @property
     def current(self):
+        if self.is_finished:
+            return 100
         return round(self.index / self.length * 100, 1)
 
     @property
@@ -27,9 +38,16 @@ class Progressbar:
 
     @property
     def remaining_index(self):
+        if self.is_finished:
+            return 0
         return self.length - self.index
 
     @property
     def estimated_remaining_seconds(self):
+        if self.is_finished:
+            return 0
         return self.average_execution_time * self.remaining_index / self.step
+
+    def finish(self):
+        self.is_finished = True
 
