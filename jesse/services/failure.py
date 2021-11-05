aabbb2c433a45dabd1fe47c23ee313a1c3c5ebb5
Saleh Mir <algo@hey.com>
2021-11-05 07:59:22 +0100

    remove comments

--- jesse/services/failure.py
+++ jesse/services/failure.py
@@ -6,27 +6,6 @@ from jesse.services.redis import sync_publish
 
 
 def register_custom_exception_handler() -> None:
-    # log_format = "%(message)s"
-    # session_id = jh.get_session_id()
-
-    # jh.make_directory('storage/logs/live-mode')
-    # jh.make_directory('storage/logs/backtest-mode')
-    # jh.make_directory('storage/logs/optimize-mode')
-    # jh.make_directory('storage/logs/collect-mode')
-    #
-    # if jh.is_live():
-    #     filename = f'storage/logs/live-mode/{jh.now(True)}--{session_id}.txt'
-    # elif jh.is_collecting_data():
-    #     filename = f'storage/logs/collect-mode/{jh.now(True)}--{session_id}.txt'
-    # elif jh.is_optimizing():
-    #     filename = f'storage/logs/optimize-mode/{jh.now(True)}--{session_id}.txt'
-    # elif jh.is_backtesting():
-    #     filename = f'storage/logs/backtest-mode/{jh.now(True)}--{session_id}.txt'
-    # else:
-    #     filename = f'storage/logs/etc.txt'
-    #
-    # logging.basicConfig(filename=filename, level=logging.INFO, filemode='w', format=log_format)
-
     # other threads
     def handle_thread_exception(args) -> None:
         if args.exc_type == SystemExit:
