fc586ae4756dcda9d860e56314bf88ccf6c0b5cf
morteza-koohgard <morteza73.koohgard@gmail.com>
2021-11-08 12:47:22 +0330

    refactor the migrator service
    fix a bug
    
    small change
    
    refactor migrator service
    
    work on migration (not finished)
    
    refactor migrator
    
    modify migrator

--- jesse/services/migrator.py
+++ jesse/services/migrator.py
@@ -5,6 +5,10 @@ from playhouse.migrate import *
 def run():
     """
     Run migrations per each table and adds new fields in case they have not been added yet.
+    note of migrator functions
+    action type: add, drop, rename, change_data_type, change_nullable, change_not_nullable
+    if actions type is rename, you must add new field with 'old_name' key.
+    to make column to not nullable, you must clean all null value of columns
     """
     migrator = PostgresqlMigrator(db)
 
@@ -23,15 +27,7 @@ def _migrate_candle(migrator):
 
     candle_columns = db.get_columns('candle')
 
-    for key, value in fields.items():
-        item_exist = any(key == item.name for item in candle_columns)
-        if item_exist:
-            print(f'{key} field previously added to candle table.')
-        else:
-            migrate(
-                migrator.add_column('candle', key, value)
-            )
-            print(f'{key} field successfully added to candle table.')
+    _check_tables(migrator, fields, candle_columns, 'candle')
 
 
 def _migrate_completed_trade(migrator):
@@ -39,15 +35,7 @@ def _migrate_completed_trade(migrator):
 
     completedtrade_columns = db.get_columns('completedtrade')
 
-    for key, value in fields.items():
-        item_exist = any(key == item.name for item in completedtrade_columns)
-        if item_exist:
-            print(f'{key} field previously added to completedtrade table.')
-        else:
-            migrate(
-                migrator.add_column('completedtrade', key, value)
-            )
-            print(f'{key} field successfully added to completedtrade table.')
+    _check_tables(migrator, fields, completedtrade_columns, 'completedtrade')
 
 
 def _migrate_daily_balance(migrator):
@@ -55,15 +43,7 @@ def _migrate_daily_balance(migrator):
 
     dailybalance_columns = db.get_columns('dailybalance')
 
-    for key, value in fields.items():
-        item_exist = any(key == item.name for item in dailybalance_columns)
-        if item_exist:
-            print(f'{key} field previously added to dailybalance table.')
-        else:
-            migrate(
-                migrator.add_column('dailybalance', key, value)
-            )
-            print(f'{key} field successfully added to dailybalance table.')
+    _check_tables(migrator, fields, dailybalance_columns, 'dailybalance')
 
 
 def _migrate_log(migrator):
@@ -71,35 +51,19 @@ def _migrate_log(migrator):
 
     log_columns = db.get_columns('log')
 
-    for key, value in fields.items():
-        item_exist = any(key == item.name for item in log_columns)
-        if not item_exist:
-            migrate(
-                migrator.add_column('log', key, value)
-            )
-            print(f'{key} field successfully added to log table.')
-        else:
-            print(f'{key} field previously added to log table.')
+    _check_tables(migrator, fields, log_columns, 'log')
 
 
 def _migrate_order(migrator):
-    fields = {
-        'trade_id': UUIDField(index=True, null=True),
-        'session_id': UUIDField(index=True, null=True),
-        'exchange_id': CharField(null=True)
-    }
+    fields = [
+        {'name': 'trade_id', 'data_type': UUIDField(index=True, null=True), 'action': 'add'},
+        {'name': 'session_id', 'data_type': UUIDField(index=True, null=True), 'action': 'add'},
+        {'name': 'exchange_id', 'data_type': CharField(null=True), 'action': 'add'}
+    ]
 
     order_columns = db.get_columns('order')
 
-    for key, value in fields.items():
-        item_exist = any(key == item.name for item in order_columns)
-        if not item_exist:
-            migrate(
-                migrator.add_column('order', key, value)
-            )
-            print(f'{key} field successfully added to order table.')
-        else:
-            print(f'{key} field previously added to order table.')
+    _check_tables(migrator, fields, order_columns, 'order')
 
 
 def _migrate_orderbook(migrator):
@@ -107,15 +71,7 @@ def _migrate_orderbook(migrator):
 
     orderbook_columns = db.get_columns('orderbook')
 
-    for key, value in fields.items():
-        item_exist = any(key == item.name for item in orderbook_columns)
-        if item_exist:
-            print(f'{key} field previously added to orderbook table.')
-        else:
-            migrate(
-                migrator.add_column('orderbook', key, value)
-            )
-            print(f'{key} field successfully added to orderbook table.')
+    _check_tables(migrator, fields, orderbook_columns, 'orderbook')
 
 
 def _migrate_ticker(migrator):
@@ -123,15 +79,7 @@ def _migrate_ticker(migrator):
 
     ticker_columns = db.get_columns('ticker')
 
-    for key, value in fields.items():
-        item_exist = any(key == item.name for item in ticker_columns)
-        if item_exist:
-            print(f'{key} field previously added to ticker table.')
-        else:
-            migrate(
-                migrator.add_column('ticker', key, value)
-            )
-            print(f'{key} field successfully added to ticker table.')
+    _check_tables(migrator, fields, ticker_columns, 'ticker')
 
 
 def _migrate_trade(migrator):
@@ -139,12 +87,58 @@ def _migrate_trade(migrator):
 
     trade_columns = db.get_columns('trade')
 
-    for key, value in fields.items():
-        item_exist = any(key == item.name for item in trade_columns)
+    _check_tables(migrator, fields, trade_columns, 'trade')
+
+
+def _check_tables(migrator, fields, columns, table):
+    for field in fields:
+        item_exist = any(field['name'] == item.name for item in columns)
+
         if item_exist:
-            print(f'{key} field previously added to trade table.')
+            if field['action'] == 'add':
+                print(f"{field['name']} field previously added to {table} table.")
+
+            elif field['action'] == 'drop':
+                migrate(
+                    migrator.drop_column(table, field['name'])
+                )
+                print(f"{field['name']} field successfully removed from {table} table.")
+
+            elif field['action'] == 'rename':
+                migrate(
+                    migrator.rename_column(table, field['name'], field['new_name'])
+                )
+                print(f"{field['name']} field successfully change to {field['new_name']} in {table} table.")
+
+            elif field['action'] == 'rename':
+                migrate(
+                    migrator.rename_column(table, field['name'], field['new_name'])
+                )
+                print(f"{field['name']} field successfully change to {field['new_name']} in {table} table.")
+
+            elif field['action'] == 'change_data_type':
+                migrate(
+                    migrator.alter_column_type(table, field['name'], field['data_type'])
+                )
+                print(f"{field['name']} field successfully change to {field['data_type']} in {table} table.")
+
+            elif field['action'] == 'change_nullable':
+                migrate(
+                    migrator.drop_not_null(table, field['name'])
+                )
+                print(f"{field['name']} field successfully change to nullable in {table} table.")
+
+            elif field['action'] == 'change_not_nullable':
+                migrate(
+                    migrator.add_not_null(table, field['name'])
+                )
+                print(f"{field['name']} field successfully change to not nullable in {table} table.")
+
         else:
-            migrate(
-                migrator.add_column('trade', key, value)
-            )
-            print(f'{key} field successfully added to trade table.')
+            if field['action'] == 'add':
+                migrate(
+                    migrator.add_column(table, field['name'], field['data_type'])
+                )
+                print(f"{field['name']} field successfully added to {table} table.")
+            else:
+                print(f"{field['name']} field not exist in {table} table.")
