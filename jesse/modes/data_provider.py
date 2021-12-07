61300d6d413f4dd89f04b8984eee2c9c0b618b1b
Saleh Mir <algo@hey.com>
2021-12-07 09:46:19 +0100

    remove is_logged_in_to_jesse_trade as it is no longer needed

--- jesse/modes/data_provider.py
+++ jesse/modes/data_provider.py
@@ -48,9 +48,7 @@ def get_candles(exchange: str, symbol: str, timeframe: str):
 
 
 def get_general_info(has_live=False) -> dict:
-
     from jesse.modes.import_candles_mode.drivers import drivers
-    from jesse.services.auth import get_access_token
 
     if has_live:
         from jesse_live.info import SUPPORTED_EXCHANGES_NAMES
@@ -61,15 +59,12 @@ def get_general_info(has_live=False) -> dict:
     exchanges = list(sorted(drivers.keys()))
     strategies_path = os.getcwd() + "/strategies/"
     strategies = list(sorted([name for name in os.listdir(strategies_path) if os.path.isdir(strategies_path + name)]))
-    is_logged_in_to_jesse_trade = get_access_token() is not None
-
 
     return {
         'exchanges': exchanges,
         'live_exchanges': live_exchanges,
         'strategies': strategies,
         'has_live_plugin_installed': has_live,
-        'is_logged_in_to_jesse_trade': is_logged_in_to_jesse_trade,
         'cpu_cores': jh.cpu_cores_count()
     }
 
