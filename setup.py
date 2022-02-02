659e31fc6e23466daf5293f09ff6c62b9f0b7839
Saleh Mir <algo@hey.com>
2022-02-02 16:41:20 +0100

    fix version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # also change in version.py
-VERSION = '0.32.0'
+VERSION = '0.32.1'
 DESCRIPTION = "A trading framework for cryptocurrencies"
 
 REQUIRED_PACKAGES = [
