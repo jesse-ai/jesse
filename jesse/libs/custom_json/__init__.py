deae30482c2ce67c232c16b52556fbf9eab0a7ba
Saleh Mir <algo@hey.com>
2022-02-10 08:04:36 +0100

    add support for bool_ values in watch_list()

--- jesse/libs/custom_json/__init__.py
+++ jesse/libs/custom_json/__init__.py
@@ -6,6 +6,8 @@ class NpEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.integer):
             return int(obj)
+        elif isinstance(obj, np.bool_):
+            return bool(obj)
         elif isinstance(obj, np.floating):
             return float(obj)
         elif isinstance(obj, np.ndarray):
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # also change in version.py
-VERSION = '0.32.8'
+VERSION = '0.32.9'
 DESCRIPTION = "A trading framework for cryptocurrencies"
 
 REQUIRED_PACKAGES = [
