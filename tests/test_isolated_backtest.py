c2429c0d1dec7d99ead78b03e4a5785c26c177f0
Saleh Mir <algo@hey.com>
2022-04-03 16:58:34 +0200

    add unit test: test_dna_method_works_in_isolated_backtest

--- tests/test_isolated_backtest.py
+++ tests/test_isolated_backtest.py
@@ -169,3 +169,86 @@ def test_store_state_app_is_reset_properly():
     research.backtest(config, routes, extra_routes, candles)
     # run the backtest for the second time and assert that the app.daily_balance is reset
     research.backtest(config, routes, extra_routes, candles)
+
+
+def test_dna_method_works_in_isolated_backtest():
+    # first define the strategy without the dna method, hence the hyperparameter defaults
+    class TestStrategy1(Strategy):
+        def before(self) -> None:
+            if self.index == 0:
+                assert self.hp['hp1'] == 70
+                assert self.hp['hp2'] == 100
+
+        def should_long(self) -> bool:
+            return False
+
+        def should_cancel(self) -> bool:
+            return True
+
+        def go_long(self):
+            pass
+
+        def hyperparameters(self):
+            return [
+                {'name': 'hp1', 'type': int, 'min': 10, 'max': 95, 'default': 70},
+                {'name': 'hp2', 'type': int, 'min': 50, 'max': 1000, 'default': 100},
+            ]
+
+    fake_candles = candles_from_close_prices([101, 102, 103, 104, 105, 106, 107, 108, 109, 110])
+    exchange_name = 'Fake Exchange'
+    symbol = 'FAKE-USDT'
+    timeframe = '1m'
+    config = {
+        'starting_balance': 10_000,
+        'fee': 0,
+        'futures_leverage': 2,
+        'futures_leverage_mode': 'cross',
+        'exchange': exchange_name,
+        'settlement_currency': 'USDT',
+        'warm_up_candles': 0
+    }
+    routes = [
+        {'exchange': exchange_name, 'strategy': TestStrategy1, 'symbol': symbol, 'timeframe': timeframe},
+    ]
+    extra_routes = []
+    candles = {
+        jh.key(exchange_name, symbol): {
+            'exchange': exchange_name,
+            'symbol': symbol,
+            'candles': fake_candles,
+        },
+    }
+
+    research.backtest(config, routes, extra_routes, candles)
+
+    # now define the strategy with the dna method
+    class TestStrategy2(Strategy):
+        def before(self) -> None:
+            if self.index == 0:
+                assert self.hp['hp1'] == 10
+                assert self.hp['hp2'] == 880
+
+        def should_long(self) -> bool:
+            return False
+
+        def should_cancel(self) -> bool:
+            return True
+
+        def go_long(self):
+            pass
+
+        def hyperparameters(self):
+            return [
+                {'name': 'hp1', 'type': int, 'min': 10, 'max': 95, 'default': 70},
+                {'name': 'hp2', 'type': int, 'min': 50, 'max': 1000, 'default': 100},
+            ]
+
+        def dna(self):
+            return "(m"
+
+    # redefine routes to use the new strategy
+    routes = [
+        {'exchange': exchange_name, 'strategy': TestStrategy2, 'symbol': symbol, 'timeframe': timeframe},
+    ]
+
+    research.backtest(config, routes, extra_routes, candles)
