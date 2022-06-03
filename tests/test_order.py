964447e89f682b568cc2affae31b868d7d508677
Saleh Mir <algo@hey.com>
2022-06-03 10:15:35 +0200

    remove bad practice unit test

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,39 +1,4 @@
-from jesse.enums import order_types, sides, order_statuses
-from jesse.models import Order
-from jesse.testing_utils import single_route_backtest, set_up
-import jesse.helpers as jh
-
-
-def test_remaining_qty_property():
-    set_up()
-
-    buy_order = Order({
-        'id': jh.generate_unique_id(),
-        'symbol': 'BTC-USDT',
-        'exchange': 'Sandbox',
-        'type': order_types.LIMIT,
-        'price': 129.33,
-        'qty': 10,
-        'filled_qty': 4,
-        'side': sides.BUY,
-        'status': order_statuses.ACTIVE,
-        'created_at': jh.now_to_timestamp(),
-    })
-    assert buy_order.remaining_qty == 6
-
-    sell_order = Order({
-        'id': jh.generate_unique_id(),
-        'symbol': 'BTC-USDT',
-        'exchange': 'Sandbox',
-        'type': order_types.LIMIT,
-        'price': 129.33,
-        'qty': -10,
-        'filled_qty': -4,
-        'side': sides.SELL,
-        'status': order_statuses.ACTIVE,
-        'created_at': jh.now_to_timestamp(),
-    })
-    assert sell_order.remaining_qty == -6
+from jesse.testing_utils import single_route_backtest
 
 
 def test_order_is_stop_loss_property():
