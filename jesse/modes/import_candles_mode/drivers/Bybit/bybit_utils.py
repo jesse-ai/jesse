99285cfc9d0ca45eecae8831815ab26fb1dc6e95
Saleh Mir <algo@hey.com>
2022-08-17 16:50:39 +0200

    refactor to use enum

--- jesse/modes/import_candles_mode/drivers/Bybit/bybit_utils.py
+++ jesse/modes/import_candles_mode/drivers/Bybit/bybit_utils.py
@@ -1,30 +1,33 @@
+from jesse.enums import timeframes
+
+
 def timeframe_to_interval(timeframe: str) -> str:
     """
     Convert a timeframe string to an interval in seconds.
     """
-    if timeframe == '1m':
+    if timeframe == timeframes.MINUTE_1:
         return '1'
-    elif timeframe == '3m':
+    elif timeframe == timeframes.MINUTE_3:
         return '3'
-    elif timeframe == '5m':
+    elif timeframe == timeframes.MINUTE_5:
         return '5'
-    elif timeframe == '15m':
+    elif timeframe == timeframes.MINUTE_15:
         return '15'
-    elif timeframe == '30m':
+    elif timeframe == timeframes.MINUTE_30:
         return '30'
-    elif timeframe == '1h':
+    elif timeframe == timeframes.HOUR_1:
         return '60'
-    elif timeframe == '2h':
+    elif timeframe == timeframes.HOUR_2:
         return '120'
-    elif timeframe == '4h':
+    elif timeframe == timeframes.HOUR_4:
         return '240'
-    elif timeframe == '6h':
+    elif timeframe == timeframes.HOUR_6:
         return '360'
-    elif timeframe == '12h':
+    elif timeframe == timeframes.HOUR_12:
         return '720'
-    elif timeframe == '1D':
+    elif timeframe == timeframes.DAY_1:
         return 'D'
-    elif timeframe == '1W':
+    elif timeframe == timeframes.WEEK_1:
         return 'W'
     else:
         raise ValueError('Invalid timeframe: {}'.format(timeframe))
@@ -35,29 +38,28 @@ def interval_to_timeframe(interval: str) -> str:
     Convert an interval in seconds to a timeframe string.
     """
     if interval == '1':
-        return '1m'
+        return timeframes.MINUTE_1
     elif interval == '3':
-        return '3m'
+        return timeframes.MINUTE_3
     elif interval == '5':
-        return '5m'
+        return timeframes.MINUTE_5
     elif interval == '15':
-        return '15m'
+        return timeframes.MINUTE_15
     elif interval == '30':
-        return '30m'
+        return timeframes.MINUTE_30
     elif interval == '60':
-        return '1h'
+        return timeframes.HOUR_1
     elif interval == '120':
-        return '2h'
+        return timeframes.HOUR_2
     elif interval == '240':
-        return '4h'
+        return timeframes.HOUR_4
     elif interval == '360':
-        return '6h'
+        return timeframes.HOUR_6
     elif interval == '720':
-        return '12h'
+        return timeframes.HOUR_12
     elif interval == 'D':
-        return '1D'
+        return timeframes.DAY_1
     elif interval == 'W':
-        return '1W'
+        return timeframes.WEEK_1
     else:
         raise ValueError('Invalid interval: {}'.format(interval))
-
