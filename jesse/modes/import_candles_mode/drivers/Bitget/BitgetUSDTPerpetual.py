5333105c5445df2b7a36fb98357d59bcd80316ef
Saleh Mir <algo@hey.com>
2022-09-07 09:39:39 +0200

    refactor Bitget driver to use a parent class

--- jesse/modes/import_candles_mode/drivers/Bitget/BitgetUSDTPerpetual.py
+++ jesse/modes/import_candles_mode/drivers/Bitget/BitgetUSDTPerpetual.py
@@ -1,67 +1,10 @@
-from typing import Union
-import requests
-from jesse.modes.import_candles_mode.drivers.interface import CandleExchange
+from .BitgetUSDTPerpetualMain import BitgetUSDTPerpetualMain
 from jesse.enums import exchanges
-from .bitget_utils import timeframe_to_interval, jesse_symbol_to_bitget_usdt_contracts_symbol
-import jesse.helpers as jh
 
 
-class BitgetUSDTPerpetual(CandleExchange):
+class BitgetUSDTPerpetual(BitgetUSDTPerpetualMain):
     def __init__(self) -> None:
         super().__init__(
             name=exchanges.BITGET_USDT_PERPETUAL,
-            count=100,
-            rate_limit_per_second=18,
-            backup_exchange_class=None
+            endpoint='https://api.bitget.com/api/mix/v1/market/candles'
         )
-
-        self.endpoint = 'https://api.bitget.com/api/mix/v1/market/candles'
-
-    def get_starting_time(self, symbol: str) -> int:
-        payload = {
-            'granularity': '1W',
-            'symbol': jesse_symbol_to_bitget_usdt_contracts_symbol(symbol),
-            'startTime': 1359291660000,
-            'endTime': jh.now(force_fresh=True)
-        }
-
-        response = requests.get(self.endpoint, params=payload)
-
-        self.validate_response(response)
-
-        data = response.json()
-
-        # since the first timestamp doesn't include all the 1m
-        # candles, let's start since the second day then
-        return int(data[1][0])
-
-    def fetch(self, symbol: str, start_timestamp: int, timeframe: str = '1m') -> Union[list, None]:
-        end_timestamp = start_timestamp + (self.count - 1) * 60000 * jh.timeframe_to_one_minutes(timeframe)
-
-        payload = {
-            'granularity': timeframe_to_interval(timeframe),
-            'symbol': jesse_symbol_to_bitget_usdt_contracts_symbol(symbol),
-            'startTime': int(start_timestamp),
-            'endTime': int(end_timestamp)
-        }
-
-        response = requests.get(self.endpoint, params=payload)
-
-        self.validate_response(response)
-
-        data = response.json()
-
-        return [
-            {
-                'id': jh.generate_unique_id(),
-                'exchange': self.name,
-                'symbol': symbol,
-                'timeframe': timeframe,
-                'timestamp': int(d[0]),
-                'open': float(d[1]),
-                'high': float(d[2]),
-                'low': float(d[3]),
-                'close': float(d[4]),
-                'volume': float(d[5])
-            } for d in data
-        ]
