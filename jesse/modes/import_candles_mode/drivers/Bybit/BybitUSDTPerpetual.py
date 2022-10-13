af71f9bf5cf73ece5ee3a39c749acb1f17d7cd29
Saleh Mir <algo@hey.com>
2022-10-13 14:46:55 +0200

    return to "bybit.com" endpoint

--- jesse/modes/import_candles_mode/drivers/Bybit/BybitUSDTPerpetual.py
+++ jesse/modes/import_candles_mode/drivers/Bybit/BybitUSDTPerpetual.py
@@ -6,5 +6,5 @@ class BybitUSDTPerpetual(BybitUSDTPerpetualMain):
     def __init__(self) -> None:
         super().__init__(
             name=exchanges.BYBIT_USDT_PERPETUAL,
-            rest_endpoint='https://api.bytick.com',
+            rest_endpoint='https://api.bybit.com',
         )
