0b5c6f2967c56ecf2b8d90898b587c832de587d9
Saleh Mir <algo@hey.com>
2022-10-25 10:10:08 +0200

    remove unused functions

--- jesse/services/auth.py
+++ jesse/services/auth.py
@@ -38,35 +38,3 @@ def get_access_token():
 
     return ENV_VALUES['LICENSE_API_TOKEN']
 
-
-def login_to_jesse_trade(email: str, password: str) -> JSONResponse:
-    response = requests.post('https://jesse.trade/api/auth', json={
-        'email': email,
-        'password': password,
-        'device_name': jh.computer_name()
-    })
-
-    if response.status_code != 200:
-        return JSONResponse({
-            'status': 'error',
-            'message': f"[{response.status_code}]: {response.json()['message']}\nPlease contact us at support@jesse.trade if this is unexpected.",
-        }, status_code=200)
-
-    access_token = response.json()['token']
-
-    os.makedirs('./storage/temp', exist_ok=True)
-    with open('storage/temp/access_token', 'w') as f:
-        f.write(access_token)
-
-    return JSONResponse({
-        'status': 'success',
-        'message': 'Successfully logged in to Jesse.Trade',
-    }, status_code=200)
-
-
-def logout_from_jesse_trade() -> JSONResponse:
-    os.remove('storage/temp/access_token')
-
-    return JSONResponse({
-        'message': 'Successfully logged out from Jesse.Trade and removed access_token file'
-    })
