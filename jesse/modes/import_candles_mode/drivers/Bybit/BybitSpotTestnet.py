612f7af4a7f3b5fd38e49f37263938d1889808ca
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-03-13 19:31:27 +0330

    Update BybitSpotTestnet exchange name

--- jesse/modes/import_candles_mode/drivers/Bybit/BybitSpotTestnet.py
+++ jesse/modes/import_candles_mode/drivers/Bybit/BybitSpotTestnet.py
@@ -5,7 +5,7 @@ from jesse.enums import exchanges
 class BybitSpotTestnet(BybitMain):
     def __init__(self) -> None:
         super().__init__(
-            name=exchanges.BYBIT_USDT_PERPETUAL_TESTNET,
+            name=exchanges.BYBIT_SPOT_TESTNET,
             rest_endpoint='https://api-testnet.bybit.com',
             category='spot',
         )
