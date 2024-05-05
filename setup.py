73ccaf4be70bba3f99adb334271179314b064dce
Saleh Mir <saleh@jesse.trade>
2024-05-05 18:56:15 +0330

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # also change in version.py
-VERSION = '0.47.4'
+VERSION = '0.48.0'
 DESCRIPTION = "A trading framework for cryptocurrencies"
 with open("requirements.txt", "r", encoding="utf-8") as f:
     REQUIRED_PACKAGES = f.read().splitlines()
