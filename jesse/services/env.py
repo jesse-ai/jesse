0c5729d54b81a10b0b8c0c10044cf56b848f25ea
Saleh Mir <saleh@jesse.trade>
2024-05-30 18:55:25 +0330

    fix tests

--- jesse/services/env.py
+++ jesse/services/env.py
@@ -8,23 +8,24 @@ sys.path.insert(0, os.getcwd())
 
 ENV_VALUES = {}
 
+if jh.is_unit_testing():
+    ENV_VALUES['POSTGRES_HOST'] = '127.0.0.1'
+    ENV_VALUES['POSTGRES_NAME'] = 'jesse_db'
+    ENV_VALUES['POSTGRES_PORT'] = '5432'
+    ENV_VALUES['POSTGRES_USERNAME'] = 'jesse_user'
+    ENV_VALUES['POSTGRES_PASSWORD'] = 'password'
+    ENV_VALUES['REDIS_HOST'] = 'localhost'
+    ENV_VALUES['REDIS_PORT'] = '6379'
+    ENV_VALUES['REDIS_DB'] = 0
+    ENV_VALUES['REDIS_PASSWORD'] = ''
+    ENV_VALUES['APP_PORT'] = 3000
+
 if jh.is_jesse_project():
     # load env
     load_dotenv()
 
     # create and expose ENV_VALUES
     ENV_VALUES = dotenv_values('.env')
-
-    if jh.is_unit_testing():
-        ENV_VALUES['POSTGRES_HOST'] = '127.0.0.1'
-        ENV_VALUES['POSTGRES_NAME'] = 'jesse_db'
-        ENV_VALUES['POSTGRES_PORT'] = '5432'
-        ENV_VALUES['POSTGRES_USERNAME'] = 'jesse_user'
-        ENV_VALUES['POSTGRES_PASSWORD'] = 'password'
-        ENV_VALUES['REDIS_HOST'] = 'localhost'
-        ENV_VALUES['REDIS_PORT'] = '6379'
-        ENV_VALUES['REDIS_DB'] = 0
-        ENV_VALUES['REDIS_PASSWORD'] = ''
 
     # validation for existence of .env file
     if len(list(ENV_VALUES.keys())) == 0:
