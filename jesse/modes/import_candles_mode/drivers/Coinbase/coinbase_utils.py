f5bc0fc22d9b5805435a7965635ece0c3ac06197
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-06-19 21:02:20 +0330

    update coinbase import candle to new api

--- jesse/modes/import_candles_mode/drivers/Coinbase/coinbase_utils.py
+++ jesse/modes/import_candles_mode/drivers/Coinbase/coinbase_utils.py
@@ -3,7 +3,21 @@ from jesse.enums import timeframes
 
 def timeframe_to_interval(timeframe: str) -> str:
     if timeframe == timeframes.MINUTE_1:
-        return '60'
+        return 'ONE_MINUTE'
+    elif timeframe == timeframes.MINUTE_5:
+        return 'FIVE_MINUTE'
+    elif timeframe == timeframes.MINUTE_15:
+        return 'FIFTEEN_MINUTE'
+    elif timeframe == timeframes.MINUTE_30:
+        return 'THIRTEEN_MINUTE'
+    elif timeframe == timeframes.HOUR_1:
+        return 'ONE_HOUR'
+    elif timeframe == timeframes.HOUR_2:
+        return 'TWO_HOUR'
+    elif timeframe == timeframes.HOUR_6:
+        return 'SIX_HOUR'
+    elif timeframe == timeframes.DAY_1:
+        return 'ONE_DAY'
     else:
         raise NotImplemented('Invalid timeframe: {}'.format(timeframe))
 
