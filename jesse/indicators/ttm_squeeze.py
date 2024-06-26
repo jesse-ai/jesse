d1d105f45da31a9aba96439a2369962a80db59e2
Saleh Mir <saleh@jesse.trade>
2024-06-26 23:49:50 +0330

    refactor: rename TTMSqueeze to ttm_squeeze and update import statements
    The code changes in this commit refactor the name of the `TTMSqueeze` indicator to `ttm_squeeze` to follow the Python naming conventions. The corresponding import statements have also been updated to reflect the new name. This change improves code readability and maintainability.

--- jesse/indicators/ttm_squeeze.py
+++ jesse/indicators/ttm_squeeze.py
@@ -1,15 +1,10 @@
-from collections import namedtuple
-
 from .bollinger_bands import bollinger_bands
 from .sma import sma
 from .trange import trange
-
 import numpy as np
 
-TTMSqueezeTuple = namedtuple('TTMSqueezeTuple', ['sqz_signal'])
 
-
-def TTMSqueeze(candles: np.ndarray, length_ttms: int = 20, bb_mult_ttms: float = 2.0, kc_mult_low_ttms: float = 2.0, source_type: str = "close") -> TTMSqueezeTuple:
+def ttm_squeeze(candles: np.ndarray, length_ttms: int = 20, bb_mult_ttms: float = 2.0, kc_mult_low_ttms: float = 2.0) -> bool:
     """
     @author daviddtech
     credits: https://www.tradingview.com/script/Mh3EmxF5-TTM-Squeeze-DaviddTech/
@@ -20,20 +15,19 @@ def TTMSqueeze(candles: np.ndarray, length_ttms: int = 20, bb_mult_ttms: float =
     :param length_ttms: int - default: 20
     :param bb_mult_ttms: float - default: 2.0
     :param kc_mult_low_ttms: float - default: 2.0
-    :param source_type: str - default: "close"
 
-    :return: TTMSqueezeTuple(sqz_signal)
+    :return: TTMSqueeze(sqz_signal)
     """
     bb_data = bollinger_bands(candles, length_ttms, bb_mult_ttms)
 
     kc_basis_ttms = sma(candles, length_ttms)
     devkc_ttms = sma(trange(candles, sequential=True), period=length_ttms)
 
-    NoSqz_ttms = bb_data.lowerband < kc_basis_ttms - devkc_ttms * \
+    no_sqz_ttms = bb_data.lowerband < kc_basis_ttms - devkc_ttms * \
         kc_mult_low_ttms or bb_data.upperband > kc_basis_ttms + devkc_ttms * kc_mult_low_ttms
 
     sqz_signal = False
-    if NoSqz_ttms:
+    if no_sqz_ttms:
         sqz_signal = True
 
-    return TTMSqueezeTuple(sqz_signal)
+    return sqz_signal
