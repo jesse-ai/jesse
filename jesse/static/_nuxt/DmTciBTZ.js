facd7e7c3878fa29fccc6d74e6e34d55f2da291f
Saleh Mir <saleh@jesse.trade>
2024-07-16 15:27:50 +0330

    update dashboard

--- jesse/static/_nuxt/DmTciBTZ.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,o as t,c as a,ag as o,a as s}from"./Dphekano.js";const c={},n={class:"text-center rounded border-2 border-dashed border-gray-300 dark:border-gray-700 bg-gray-50 dark:bg-gray-800 py-4 text-sm"},d=s("span",{class:"text-gray-400"}," Empty ",-1);function _(e,g){return t(),a("div",n,[o(e.$slots,"default",{},()=>[d])])}const p=r(c,[["render",_]]);export{p as _};
--- jesse/static/_nuxt/IFmokRxB.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,ag as t}from"./Dphekano.js";const s={};function a(e,n){return t(e.$slots,"default")}const _=r(s,[["render",a]]);export{_ as default};
