15194ce4cb8971c51272122c208b6ee3bfe6ad37
Saleh Mir <saleh@jesse.trade>
2024-07-25 18:54:36 +0330

    v1 release baby

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # also change in version.py
-VERSION = '0.49.1'
+VERSION = '1.0.0'
 DESCRIPTION = "A trading framework for cryptocurrencies"
 with open("requirements.txt", "r", encoding="utf-8") as f:
     REQUIRED_PACKAGES = f.read().splitlines()
