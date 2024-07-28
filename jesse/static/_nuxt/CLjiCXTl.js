b1d3a063d91380cf95292a5194aed5d4b6ecc1e2
Saleh Mir <saleh@jesse.trade>
2024-07-29 00:43:36 +0330

    update dashboard

---
+++ jesse/static/_nuxt/CLjiCXTl.js
@@ -0,0 +1,1 @@
+import{u,bc as l}from"./rHDmcL48.js";function f(e,a){const{title:r,titleTemplate:n,...o}=e;return u({title:r,titleTemplate:n,_flatMeta:o},{...a,transform(t){const s=l({...t._flatMeta});return delete t._flatMeta,{...t,meta:s}}})}export{f as u};
