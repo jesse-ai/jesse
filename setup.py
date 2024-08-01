5dcbf6e080917656fa958177f30994d0894d7833
Saleh Mir <saleh@jesse.trade>
2024-08-01 18:35:53 +0330

    1.0.8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # also change in version.py
-VERSION = '1.0.7'
+VERSION = '1.0.8'
 DESCRIPTION = "A trading framework for cryptocurrencies"
 with open("requirements.txt", "r", encoding="utf-8") as f:
     REQUIRED_PACKAGES = f.read().splitlines()
