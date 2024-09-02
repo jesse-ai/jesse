25de33e233f61a2cb78f2460bcac664e4f91bef9
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-09-02 17:35:46 +0330

    fix typo

--- jesse/modes/import_candles_mode/drivers/Gate/GateUSDTPerpetualTestnet.py
+++ jesse/modes/import_candles_mode/drivers/Gate/GateUSDTPerpetualTestnet.py
@@ -5,6 +5,6 @@ from jesse.enums import exchanges
 class GateUSDTPerpetualTestnet(GateUSDTMain):
     def __init__(self) -> None:
         super().__init__(
-            name=exchanges.GATE_USDT_PERPETUAL_FUTURES_TESTNET,
+            name=exchanges.GATE_USDT_PERPETUAL_TESTNET,
             rest_endpoint='https://fx-api-testnet.gateio.ws/api/v4/futures'
         )
