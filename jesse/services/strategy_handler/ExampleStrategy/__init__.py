966ffb3b50018054963d384904387956be8cb19a
Saleh Mir <saleh@jesse.trade>
2024-10-05 20:29:45 +0330

    Refactor ExampleStrategy class

--- jesse/services/strategy_handler/ExampleStrategy/__init__.py
+++ jesse/services/strategy_handler/ExampleStrategy/__init__.py
@@ -1,4 +1,4 @@
-from jesse.strategies import Strategy, cached
+from jesse.strategies import Strategy
 import jesse.indicators as ta
 from jesse import utils
 
@@ -7,12 +7,12 @@ class ExampleStrategy(Strategy):
     def should_long(self) -> bool:
         return False
 
-    def go_long(self):
-        pass
-
     def should_short(self) -> bool:
         # For futures trading only
         return False
+        
+    def go_long(self):
+        pass
 
     def go_short(self):
         # For futures trading only
