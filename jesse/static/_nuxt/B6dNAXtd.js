90b5d5775870e43e88c456f0ac52817dc5a90c37
Saleh Mir <saleh@jesse.trade>
2024-11-04 14:17:13 +0330

    update dashboard

---
+++ jesse/static/_nuxt/B6dNAXtd.js
@@ -0,0 +1,1 @@
+import{a as r,x as t}from"./hBiBYkeL.js";const s={};function a(e,n){return t(e.$slots,"default")}const c=r(s,[["render",a]]);export{c as default};
---
+++ jesse/static/_nuxt/BjmrteOn.js
@@ -0,0 +1,1 @@
+import{bs as a,G as e,bt as t}from"./hBiBYkeL.js";const n=a((r,o)=>{if(e().plan==="free"||e().plan==="guest")return t("/")});export{n as default};
