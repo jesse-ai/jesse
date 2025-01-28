a38ac9dae25261305a66156d03e6e373a3001e26
Saleh Mir <saleh@jesse.trade>
2025-01-28 17:39:58 +0330

    Restore TA-Lib installation script to version 0.6.1
    - Update script to use TA-Lib 0.6.1 source archive
    - Modify extraction path to match new version
    - Re-add sed modification for floating-point precision

--- docker_build_helpers/install_ta-lib.sh
+++ docker_build_helpers/install_ta-lib.sh
@@ -5,8 +5,8 @@ else
 fi
 echo "Installing to ${INSTALL_LOC}"
 if [ ! -f "${INSTALL_LOC}/lib/libta_lib.a" ]; then
-  tar zxvf ta-lib-0.4.0-src.tar.gz
-  cd ta-lib \
+  tar zxvf ta-lib-0.6.1-src.tar.gz
+  cd ta-lib-0.6.1 \
   && sed -i.bak "s|0.00000001|0.000000000000000001 |g" src/ta_func/ta_utility.h \
   && ./configure --prefix=${INSTALL_LOC}/ \
   && make \
