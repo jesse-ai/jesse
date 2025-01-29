2658889fdf5e1abee45dc53f5a08bad6be125752
Saleh Mir <saleh@jesse.trade>
2025-01-29 22:06:41 +0330

    Fix TA-Lib installation script floating-point precision sed command
    - Remove extra whitespace in sed replacement for ta_utility.h
    - Ensure precise floating-point modification without introducing unintended spaces

--- docker_build_helpers/install_ta-lib.sh
+++ docker_build_helpers/install_ta-lib.sh
@@ -7,7 +7,7 @@ echo "Installing to ${INSTALL_LOC}"
 if [ ! -f "${INSTALL_LOC}/lib/libta_lib.a" ]; then
   tar zxvf ta-lib-0.6.1-src.tar.gz
   cd ta-lib-0.6.1 \
-  && sed -i.bak "s|0.00000001|0.000000000000000001 |g" src/ta_func/ta_utility.h \
+  && sed -i.bak "s|0.00000001|0.000000000000000001|g" src/ta_func/ta_utility.h \
   && ./configure --prefix=${INSTALL_LOC}/ \
   && make \
   && which sudo && sudo make install || make install \
