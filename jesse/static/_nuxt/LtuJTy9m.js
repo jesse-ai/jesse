a83670d85827af7163affcdcf12b724098658c36
Saleh Mir <saleh@jesse.trade>
2025-01-30 23:55:10 +0330

    Update dashboard files

---
+++ jesse/static/_nuxt/LtuJTy9m.js
@@ -0,0 +1,1 @@
+import{a as e,o,c as t,b as s,s as c}from"./BE3918HX.js";const n={},r={class:"py-10 mx-auto"},l={class:"mx-auto max-w-7xl px-2 md:px-8"},_={class:"max-w-[600px] mx-auto"};function d(a,m){return o(),t("div",r,[s("main",null,[s("div",l,[s("div",_,[c(a.$slots,"default")])])])])}const x=e(n,[["render",d]]);export{x as S};
---
+++ jesse/static/_nuxt/RWw3KwLs.js
@@ -0,0 +1,1 @@
+import{a as r,s as t}from"./BE3918HX.js";const s={};function a(e,n){return t(e.$slots,"default")}const c=r(s,[["render",a]]);export{c as default};
