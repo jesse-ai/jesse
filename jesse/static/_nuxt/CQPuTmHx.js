3c15cc97b922d0aeb399c4bf71c6f8ae9acff4a9
Saleh Mir <saleh@jesse.trade>
2025-02-07 23:08:48 +0330

    Update dashboard files

--- jesse/static/_nuxt/CQPuTmHx.js
+++
@@ -1,1 +0,0 @@
-import{bE as a,D as e,S as t}from"./BHbRGkBn.js";const s=a((r,o)=>{if(e().plan==="free"||e().plan==="guest")return t("/")});export{s as default};
---
+++ jesse/static/_nuxt/qnwMB9aY.js
@@ -0,0 +1,1 @@
+import{bG as a,M as e,$ as t}from"./Bxvlha2F.js";const s=a((r,o)=>{if(e().plan==="free"||e().plan==="guest")return t("/")});export{s as default};
