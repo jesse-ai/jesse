6d181f74ccdc401e2cead095f9f771fc90228829
Saleh Mir <saleh@jesse.trade>
2025-03-06 17:59:20 +0330

    Create dedicated WebSocket controller and refactor WebSocket route
    - Move WebSocket-related code from `__init__.py` to new `websocket_controller.py`
    - Use APIRouter for better route organization and modularity
    - Add WebSocket router to `__init__.py`
    - Maintain existing WebSocket connection and message handling logic
    - Improve code structure by separating WebSocket-specific functionality

--- jesse/__init__.py
+++ jesse/__init__.py
@@ -1,25 +1,18 @@
-import asyncio
-import json
 import warnings
 import click
 import pkg_resources
-from fastapi import Query
-from starlette.websockets import WebSocket, WebSocketDisconnect
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from jesse.services import auth as authenticator
 from jesse.services.multiprocessing import process_manager
-from jesse.services.redis import async_redis
 from jesse.services.web import fastapi_app
 import uvicorn
-from asyncio import Queue
 import jesse.helpers as jh
 import time
 
 # to silent stupid pandas warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-
+# get the jesse directory
 JESSE_DIR = pkg_resources.resource_filename(__name__, '')
 
 
@@ -28,70 +21,6 @@ JESSE_DIR = pkg_resources.resource_filename(__name__, '')
 async def index():
     return FileResponse(f"{JESSE_DIR}/static/index.html")
 
-
-# Import and include routers
-from jesse.controllers.optimization_controller import router as optimization_router
-from jesse.controllers.exchange_controller import router as exchange_router
-from jesse.controllers.backtest_controller import router as backtest_router
-from jesse.controllers.candles_controller import router as candles_router
-from jesse.controllers.strategy_controller import router as strategy_router
-from jesse.controllers.auth_controller import router as auth_router
-from jesse.controllers.config_controller import router as config_router
-from jesse.controllers.notification_controller import router as notification_router
-from jesse.controllers.system_controller import router as system_router
-from jesse.controllers.file_controller import router as file_router
-
-# register routers
-fastapi_app.include_router(optimization_router)
-fastapi_app.include_router(exchange_router)
-fastapi_app.include_router(backtest_router)
-fastapi_app.include_router(candles_router)
-fastapi_app.include_router(strategy_router)
-fastapi_app.include_router(auth_router)
-fastapi_app.include_router(config_router)
-fastapi_app.include_router(notification_router)
-fastapi_app.include_router(system_router)
-fastapi_app.include_router(file_router)
-
-
-@fastapi_app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket, token: str = Query(...)):
-    from jesse.services.env import ENV_VALUES
-
-    if not authenticator.is_valid_token(token):
-        return
-
-    await websocket.accept()
-
-    queue = Queue()
-    ch, = await async_redis.psubscribe(f"{ENV_VALUES['APP_PORT']}:channel:*")
-
-    async def echo(q):
-        try:
-            while True:
-                msg = await q.get()
-                msg = json.loads(msg)
-                msg['id'] = process_manager.get_client_id(msg['id'])
-                await websocket.send_json(msg)
-        except WebSocketDisconnect:
-            await async_redis.punsubscribe(f"{ENV_VALUES['APP_PORT']}:channel:*")
-            print(jh.color('WebSocket disconnected', 'yellow'))
-        except Exception as e:
-            print(jh.color(str(e), 'red'))
-
-    async def reader(channel, q):
-        async for ch, message in channel.iter():
-            await q.put(message)
-
-    asyncio.get_running_loop().create_task(reader(ch, queue))
-    asyncio.get_running_loop().create_task(echo(queue))
-
-    try:
-        while True:
-            await websocket.receive_text()
-    except WebSocketDisconnect:
-        await async_redis.punsubscribe(f"{ENV_VALUES['APP_PORT']}:channel:*")
-        print(jh.color('WebSocket disconnected', 'yellow'))
 
 # create a Click group
 @click.group()
@@ -149,12 +78,43 @@ def shutdown_event():
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Live Plugin Endpoints
+# Routes
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+from jesse.controllers.websocket_controller import router as websocket_router
+from jesse.controllers.optimization_controller import router as optimization_router
+from jesse.controllers.exchange_controller import router as exchange_router
+from jesse.controllers.backtest_controller import router as backtest_router
+from jesse.controllers.candles_controller import router as candles_router
+from jesse.controllers.strategy_controller import router as strategy_router
+from jesse.controllers.auth_controller import router as auth_router
+from jesse.controllers.config_controller import router as config_router
+from jesse.controllers.notification_controller import router as notification_router
+from jesse.controllers.system_controller import router as system_router
+from jesse.controllers.file_controller import router as file_router
+
+# register routers
+fastapi_app.include_router(websocket_router)
+fastapi_app.include_router(optimization_router)
+fastapi_app.include_router(exchange_router)
+fastapi_app.include_router(backtest_router)
+fastapi_app.include_router(candles_router)
+fastapi_app.include_router(strategy_router)
+fastapi_app.include_router(auth_router)
+fastapi_app.include_router(config_router)
+fastapi_app.include_router(notification_router)
+fastapi_app.include_router(system_router)
+fastapi_app.include_router(file_router)
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Live Trade Plugin
 # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 if jh.has_live_trade_plugin():
     from jesse.controllers.live_controller import router as live_router
     fastapi_app.include_router(live_router)
 
 
-# Mount static files.Must be loaded at the end to prevent overlapping with API endpoints
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Static Files (Must be loaded at the end to prevent overlapping with API endpoints)
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
 fastapi_app.mount("/", StaticFiles(directory=f"{JESSE_DIR}/static"), name="static")
