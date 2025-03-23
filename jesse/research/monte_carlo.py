e78502e4c1630c8961459be85ec634d0a20d1522
yakir4123 <yakir@blockaid.co>
2025-03-23 10:46:05 +0200

    work on automate backtest for monte carlo

--- jesse/research/monte_carlo.py
+++ jesse/research/monte_carlo.py
@@ -1,5 +1,4 @@
 import functools
-import multiprocessing
 from typing import List, Dict
 
 import jesse.helpers as jh
@@ -16,10 +15,18 @@ def monte_carlo(
     hyperparameters: dict = None,
     fast_mode: bool = True,
     scenarios: int = 1000,
-    cpu_cores: int = 0,
+    progress_bar: bool = False,
+    with_candles_pipeline: bool = True,
 ) -> list[dict]:
+    if progress_bar:
+        if jh.is_notebook():
+            from tqdm.notebook import tqdm
+        else:
+            from tqdm import tqdm
+        scenarios_list = tqdm(range(scenarios))
+    else:
+        scenarios_list = range(scenarios)
 
-    cpu_cores = cpu_cores or multiprocessing.cpu_count()
     _backtest = functools.partial(
         backtest,
         config=config,
@@ -30,15 +37,18 @@ def monte_carlo(
         generate_equity_curve=True,
         hyperparameters=hyperparameters,
         fast_mode=fast_mode,
+        with_candles_pipeline=with_candles_pipeline
     )
 
     def allow_failures_backtests(*args, **kwargs):
         try:
             return _backtest(*args, **kwargs)
         except Exception as e:
-            return {'exception': e}
+            return {"exception": e}
 
-    with multiprocessing.Pool(processes=cpu_cores) as pool:
-        benchmarks = [benchmark] + [False] * (scenarios - 1)
-        results = pool.map(allow_failures_backtests, benchmarks)
-    return results
+    return [
+        allow_failures_backtests(
+            benchmark=benchmark and i == 0, with_candles_pipeline=i != 0
+        )
+        for i in scenarios_list
+    ]
