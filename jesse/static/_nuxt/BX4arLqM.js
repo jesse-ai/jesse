504e24df02836c89e8b9e790f19b68cf0e77bbf7
Saleh Mir <saleh@jesse.trade>
2025-06-30 22:24:50 +0330

    Update dashboard files

---
+++ jesse/static/_nuxt/BX4arLqM.js
@@ -0,0 +1,1 @@
+import{u,bG as l}from"./CgNmNnAO.js";function f(e,a){const{title:r,titleTemplate:n,...o}=e;return u({title:r,titleTemplate:n,_flatMeta:o},{...a,transform(t){const s=l({...t._flatMeta});return delete t._flatMeta,{...t,meta:s}}})}export{f as u};
