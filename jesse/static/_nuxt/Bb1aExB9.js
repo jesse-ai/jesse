a2aafb3c935cc2b147d9cfedf6c23281ff07ef9d
Saleh Mir <saleh@jesse.trade>
2025-07-17 16:04:15 +0330

    1.10.4 Update Dashboard files

---
+++ jesse/static/_nuxt/Bb1aExB9.js
@@ -0,0 +1,1 @@
+import{a as r,B as t}from"./Jjv4biHX.js";const s={};function a(e,n){return t(e.$slots,"default")}const c=r(s,[["render",a]]);export{c as default};
---
+++ jesse/static/_nuxt/DKCBwUyO.js
@@ -0,0 +1,1 @@
+import{bH as a,O as e,a1 as t}from"./Jjv4biHX.js";const s=a((r,o)=>{if(e().plan==="free"||e().plan==="guest")return t("/")});export{s as default};
