c7941b3aad799871b198c35eac99d9eb8dfe2421
Saleh Mir <saleh@jesse.trade>
2025-09-05 19:41:04 +0330

    Refactor import statement for Monte Carlo functions to simplify module structure and improve readability.

--- jesse/research/__init__.py
+++ jesse/research/__init__.py
@@ -1,4 +1,4 @@
 from .candles import get_candles, store_candles, fake_candle, fake_range_candles, candles_from_close_prices
 from .backtest import backtest
-from .monte_carlo.monte_carlo import monte_carlo_trades, monte_carlo_candles
+from .monte_carlo import monte_carlo_trades, monte_carlo_candles
 from .import_candles import import_candles
