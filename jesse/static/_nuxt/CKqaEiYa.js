0ebbdcf952008d1b68c82e79ae4c8854c1d33c19
Saleh Mir <saleh@jesse.trade>
2026-05-09 20:50:11 +0330

    2.1.1

--- jesse/static/_nuxt/CKqaEiYa.js
+++
@@ -1,1 +0,0 @@
-import{a1 as l,a2 as m}from"./SPdz97m5.js";function f(e,a){const{title:r,titleTemplate:n,...o}=e;return l({title:r,titleTemplate:n,_flatMeta:o},{...a,transform(t){const s=m({...t._flatMeta});return delete t._flatMeta,{...t,meta:s}}})}export{f as u};
