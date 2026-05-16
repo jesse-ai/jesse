19e2ba6c8da6e447b00d9dd32b516acf313c2d42
Saleh Mir <saleh@jesse.trade>
2026-05-16 15:10:45 +0330

    "2.1.2"

--- jesse/static/_nuxt/BZDD7Rft.js
+++
@@ -1,1 +0,0 @@
-import{a1 as l,a2 as m}from"./okI7giZ-.js";function f(e,a){const{title:r,titleTemplate:n,...o}=e;return l({title:r,titleTemplate:n,_flatMeta:o},{...a,transform(t){const s=m({...t._flatMeta});return delete t._flatMeta,{...t,meta:s}}})}export{f as u};
---
+++ jesse/static/_nuxt/By2KEGS6.js
@@ -0,0 +1,1 @@
+import{a3 as e,a4 as a,I as t}from"./9pXJQRv2.js";const s=e((r,o)=>{if(a().plan==="free"||a().plan==="guest")return t("/")});export{s as default};
