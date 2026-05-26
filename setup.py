3c6d9d8a9d34f4887799c9126bfc357ef6776634
Saleh Mir <saleh@jesse.trade>
2026-05-26 18:52:14 +0330

    "2.2.0"

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from setuptools import setup, find_packages
 
 # also change in version.py
-VERSION = "2.1.5"
+VERSION = "2.2.0"
 DESCRIPTION = "A trading framework for cryptocurrencies"
 BASE_DIR = Path(__file__).resolve().parent
 
